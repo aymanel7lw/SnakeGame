08fcb7eca73ce57da97ec65f71bd6ccc4ba4d87c
Ayman <aymanel7lw@yahoo.com>
2023-07-07 18:04:44 +0300

    re-named sanke-game file w/ main and add high score tracking in txt file

--- score.py
+++ score.py
@@ -1,12 +1,14 @@
 from turtle import Turtle
 
 ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 16, "normal")
 
 class Score (Turtle):
     def __init__ (self):
         super().__init__()
         self.score = 0
+        with open ("data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -14,13 +16,19 @@ class Score (Turtle):
         self.update_scoreboard()
         
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", False,align = ALIGNMENT, font = FONT )       
-        
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False,align = ALIGNMENT, font = FONT )       
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}") 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
     
     def game_over(self):
-        self.goto(0,0)
+        self.reset()
+        self.home()
         self.write("Game Over", False, align = ALIGNMENT, font = FONT )
